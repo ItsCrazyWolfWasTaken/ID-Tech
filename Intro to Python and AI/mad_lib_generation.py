1359265a28a0d2f6eb817fda3b8536e04d836b7e
Ben Kiev <146984941+ItsCrazyWolfWasTaken@users.noreply.github.com>
2024-07-17 11:51:04 -0400

    Add files via upload

---
+++ Intro to Python and AI/mad_lib_generation.py
@@ -0,0 +1,28 @@
+from openai import OpenAI
+
+client = OpenAI(
+    api_key = "sk-proj-zb7dRzvTbArDGy6is6oYT3BlbkFJl6JISDKrE1OmOTXMcgbO"
+)
+
+subject = ["emotion", "color", "family member", "country", "state", "noun", "food", "ocean", "verb", "type of metal", "job", "animal", "adjective", "instrument", "musician", "name", "building"]
+
+list_of_words = []
+string_of_words = ""
+
+for x in range(17):
+    get_input = input("Please type in a(n) " + str(subject[x]) + ": ")
+    list_of_words.append(get_input)
+
+system_data = [
+    {"role": "system", "content": "Generate a funny two-paragraph Mad Lib story using the words."},
+    {"role": "user", "content": string_of_words.join(list_of_words)}
+]
+
+response = client.chat.completions.create(
+    model="gpt-3.5-turbo",
+    messages=system_data
+)
+
+assistant_response = response.choices[0].message.content
+system_data.append({"role": "assistant", "content": assistant_response})
+print("Assistant: " + assistant_response)
